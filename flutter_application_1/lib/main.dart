fa5e0f32b7ddeb08d74271c9d3de0e619516913a
Gabriel Alexandre <gabriel.servicos5@gmail.com>
2023-08-03 22:35:14 -0300

    Alteração #3
    Adicionado containers, texto, StatelessWidget, ListView e cores.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -19,39 +19,79 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
+          backgroundColor: Colors.blueAccent,
           title: const Text('Tarefas'),
         ),
-        body: Container(
-          child: Stack(children: [
-            Container(
-              color: Colors.blue,
-              height: 140,
-            ),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
-                  ),
-                  const Text('Aprender Flutter'),
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: Icon(Icons.arrow_drop_up_rounded))
-                ],
-              ),
-            ),
-          ]),
+        body: ListView(
+          children: [
+            Task('Aprender Flutter'),
+            Task('Andar de Bike'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          backgroundColor: Colors.amberAccent,
+          backgroundColor: Colors.purple[100],
         ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Container(
+                  width: 200,
+                  child: Text(
+                    nome,
+                    style: TextStyle(
+                      fontSize: 20,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.blueAccent)),
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.arrow_drop_up_rounded,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
